927618b3c2865205cb729de1ddf1fbb771867b42
Pranjal Singh <155311442+06Pranjal@users.noreply.github.com>
2026-04-09 19:50:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Adding One/adding-one.java
@@ -0,0 +1,23 @@
+class Solution {
+    Vector<Integer> addOne(int[] arr) {
+        
+        Vector<Integer> res = new Vector<>();
+        
+        // Copy array
+        for (int i = 0; i < arr.length; i++) {
+            res.add(arr[i]);
+        }
+
+        // Add one from last
+        for (int i = res.size() - 1; i >= 0; i--) {
+            int sum = res.get(i) + 1;
+            res.set(i, sum % 10);
+
+            if (sum < 10) return res; // no carry → done
+        }
+
+        // If carry still remains
+        res.add(0, 1);
+        return res;
+    }
+}
